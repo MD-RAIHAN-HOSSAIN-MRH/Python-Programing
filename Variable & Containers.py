7b34193baf204484cb50b0f031669df3e28027e4
MD. RAIHAN HOSSAIN <cit.mdraihanhossain@gmail.com>
2026-01-02 22:34:26 +0600

    Create Variable & Containers.py

---
+++ Variable & Containers.py
@@ -0,0 +1,17 @@
+# Variables
+# Containers
+
+# x is the name of a variable!
+x = "Hello World"
+print(x)
+print(type(x))
+
+# y is the name of a variable!
+y = 2024
+print(y)
+print(type(y))
+
+# z is the name of a variable!
+z = False
+print(z)
+print(type(z))
