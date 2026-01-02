8c70f776659584a024807cf760b32af3013f90ad
MD. RAIHAN HOSSAIN <cit.mdraihanhossain@gmail.com>
2026-01-02 22:32:18 +0600

    Update string example in string data type.py

---
+++ string data type.py
@@ -0,0 +1,5 @@
+# This is string data type
+print(type("Creative")) # This is a string data type
+print(type(2024)) # This is an integer data type
+print(type(2024.1)) # This is a floating point number # Decimal Value
+print(type(False))   # This is a boolean data type --> True/False
